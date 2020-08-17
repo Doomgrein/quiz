112a9ba0283310cbb42668de16c44e032b99607c
Doomgrein <elwarrior@bk.ru>
2020-08-17 22:28:16 +0700

    structure for mainpage

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<div class="content">
+		Here will be content of Index Page (with class)
+	</div>
+
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer">
+	Footer with class
+</div>
